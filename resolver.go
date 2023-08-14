f99ec1a062fdc3fcdab5150519e1ecadb12c2c86
qqxhb <30866940+qqxhb@users.noreply.github.com>
2023-08-14 14:54:47 +0800

    feat: fix conn pool (#112)
    * feat: fix conn pool
    
    * feat: pool
    
    * feat: pool to sql db
    
    * feat: pool

--- resolver.go
+++ resolver.go
@@ -1,6 +1,8 @@
 package dbresolver
 
 import (
+	"database/sql"
+
 	"gorm.io/gorm"
 )
 
@@ -54,10 +56,33 @@ func (r *resolver) call(fc func(connPool gorm.ConnPool) error) error {
 		}
 	}
 
-	for _, r := range r.replicas {
-		if err := fc(r); err != nil {
+	for _, re := range r.replicas {
+		if err := fc(re); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func ConnPoolToSqlDB(connPool gorm.ConnPool, db *gorm.DB) (*sql.DB, bool) {
+	if connPool == nil {
+		return nil, false
+	}
+	if sqldb, ok := connPool.(*sql.DB); ok && sqldb != nil {
+		return sqldb, true
+	}
+	if dbConnector, ok := connPool.(gorm.GetDBConnector); ok && dbConnector != nil {
+		if sqldb, err := dbConnector.GetDBConn(); sqldb != nil || err != nil {
+			return sqldb, true
+		}
+	}
+	if db == nil {
+		return nil, false
+	}
+	if pool, ok := connPool.(gorm.GetDBConnectorWithContext); ok {
+		if sqldb, err := pool.GetDBConnWithContext(db); sqldb != nil || err != nil {
+			return sqldb, true
+		}
+	}
+	return nil, false
 }
